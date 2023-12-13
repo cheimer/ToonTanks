7e75ccb52eb1ba423b24bb685856dbc821aef7cd
cheimer <cheimer1337@gmail.com>
2023-12-13 14:29:34 +0900

    초기 커밋.

---
+++ Source/ToonTanks/Private/BasePawn.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasePawn.h"
+
+ABasePawn::ABasePawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ABasePawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABasePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
